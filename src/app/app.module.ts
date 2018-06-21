8cf112bea8722f362a1cc47c66c26db01f1b6ba7
Marcello Passos <marcellogpassos@gmail.com>
2018-06-21 18:15:32 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarComponent } from './components/shared/sidebar/sidebar.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoMaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
 @NgModule({
+  entryComponents: [
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent
+  ],
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    NgbModule.forRoot(),
+    BrowserAnimationsModule,
+    DemoMaterialModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
